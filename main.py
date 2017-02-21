644575aac97b85ce22277043446a75ccb64f4c1f
alantrrs <alan.trrs.ngls@gmail.com>
2017-02-20 21:55:42 -0800

    Save accuracy table + model

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import mnist_loader
 import network
 
+epochs = 10
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 net = network.Network([784, 30, 10])
 ev_cost, ev_acc, tr_cost, training_accuracy = net.SGD(
-    training_data, 1, 10, 3.0,
+    training_data, epochs, 10, 3.0,
     evaluation_data=test_data,
     monitor_training_accuracy=True
 )
 
-# Save results
+# Save model
+net.save('/workspace/network.json')
+
+# Save overall
 import json
 results = json.dumps({
     'metric': 'Accuracy',
-    'value': float(training_accuracy[0])/50000.0
+    'value': float(training_accuracy[epochs - 1])/50000.0
 })
 
 f = open('/workspace/overall.json','w')
 f.write(results)
 f.close()
+
+# Save training accuracy
+table = [['Epoch', 'Accuracy']]
+for row, acc in enumerate(training_accuracy):
+    table.append([row, float(acc/5000.0)])
+t = open('/workspace/accuracy.json', 'w')
+t.write(json.dumps(table))
+t.close()
